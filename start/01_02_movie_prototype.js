a68e0e6e6d0f7a227e7b24a247bc7198efdd59b8
Chanel Kim (she/her) <kimc889@newschool.edu>
2023-09-09 00:49:15 +0000

    challenge 2 submission

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,22 @@
 // Write your code here
+//PROTOTYPE CHAIN: Js uses objects to construct inheritance
+//Step 1: create a movie class
+class Movie {
+  constructor( title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+
+  //Step 2: create function for movie prototype
+  overview() {
+    return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}.`
+  }
+
+};
+
+//Step 3: test
+const ExampleMovie = new Movie("Movie Title", "Director Name", "Genre Type", "19XX", 10);
+console.log(ExampleMovie.overview());
